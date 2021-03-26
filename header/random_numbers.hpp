cc787980c8704395686fc1555eb3d7519573acb3
anhquan7826 <anhquan7826@gmail.com>
2021-03-27 01:33:21 +0700

    new header. used Fisher-Yates shuffle

---
+++ header/random_numbers.hpp
@@ -0,0 +1,6 @@
+#ifndef RANDOM_NUMBERS
+#define RANDOM_NUMBERS
+
+void random_numbers(const int range, int &a, int &b, int &c, int &d);
+
+#endif
---
+++ source/random_numbers.cpp
@@ -0,0 +1,20 @@
+#include "random_numbers.hpp"
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+void random_numbers(const int range, int &a, int &b, int &c, int &d) {
+    int array[range];
+    for (int i=0; i<range; i++) {
+        array[i] = i;
+    }
+    for (int i=range-1; i>0; i--) {
+        int a = rand()%i;
+        swap(array[i], array[a]);
+    }
+    a = array[0];
+    b = array[1];
+    c = array[2];
+    d = array[3];
+}
